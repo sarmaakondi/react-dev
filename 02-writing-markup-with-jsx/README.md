477bb628b1ee62e2dbebfcd759595516bd5b4b41
sarmaakondi <avnmsarma@gmail.com>
2024-12-29 21:19:16 +1100

    Writing markup with JSX

---
+++ 02-writing-markup-with-jsx/README.md
@@ -0,0 +1,7 @@
+# Visit below to learn more
+
+    https://react.dev/learn#writing-markup-with-jsx
+
+## Snapshot
+
+![writing markup with JSX](public/writing-markup-with-jsx.png)
---
+++ 02-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,12 @@
+import AboutPage from "./components/AboutPage";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <AboutPage />
+        </div>
+    );
+}
+
+export default App;
